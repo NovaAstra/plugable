72ef8d54c1e88c43d91fe389179bb808f575c5e8
AugustGao <August.Gao@mspbots.ai>
2024-08-05 19:01:34 +0800

    main

--- index.ts
+++ index.ts
@@ -2,10 +2,6 @@ export type MaybePromise<T> = T | Promise<T>;
 
 export type Plugin = {}
 
-export function fetchSortPlugins<P extends Plugin>() {
-
-}
-
 export class Factory {
     private readonly plugins: Readonly<Plugin[]> = []
 
