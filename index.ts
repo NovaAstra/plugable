feb7e43cbfc70cd24b03e7ac0cc5c899f12720c3
NovaAstra <NovaAstra@outlook.com>
2024-08-05 22:34:07 +0800

    main

--- index.ts
+++ index.ts
@@ -1,11 +1,29 @@
 export type MaybePromise<T> = T | Promise<T>;
 
+export type Hook = {}
+
+export type API = {}
+
 export type Plugin = {}
 
-export class Factory {
-    private readonly plugins: Readonly<Plugin[]> = []
+export function fetchOrCreate() {
+
+}
+
+export abstract class Factory {
+    public abstract name: Symbol
+
+    public readonly plugins: Readonly<Plugin[]> = []
 
     public constructor(plugins: Plugin[]) {
         this.plugins = [...plugins]
     }
+
+    public abstract tap
+
+    public abstract call
+}
+
+export class Plugable {
+    public constructor() { }
 }
