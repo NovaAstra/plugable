e3dc37222077ef9a1c223ddd4dfd81697e00fccc
AugustGao <August.Gao@mspbots.ai>
2024-08-06 19:01:48 +0800

    main

--- index.ts
+++ index.ts
@@ -1,29 +1,5 @@
 export type MaybePromise<T> = T | Promise<T>;
 
-export type Hook = {}
+export abstract class HookFactory {
 
-export type API = {}
-
-export type Plugin = {}
-
-export function fetchOrCreate() {
-
-}
-
-export abstract class Factory {
-    public abstract name: Symbol
-
-    public readonly plugins: Readonly<Plugin[]> = []
-
-    public constructor(plugins: Plugin[]) {
-        this.plugins = [...plugins]
-    }
-
-    public abstract tap
-
-    public abstract call
-}
-
-export class Plugable {
-    public constructor() { }
 }
