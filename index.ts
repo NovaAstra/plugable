4a152f907304c3ec8eb1b7a715b56e12850d59fe
NovaAstra <NovaAstra@outlook.com>
2024-08-06 21:56:18 +0800

    main

--- index.ts
+++ index.ts
@@ -1,5 +1,26 @@
-export type MaybePromise<T> = T | Promise<T>;
+export type ReturnType<T> = T extends Promise<infer R> ? Promise<R> : T;
 
-export abstract class HookFactory {
+export type ArgsType<T> = T extends unknown[] ? T : never;
 
+export function createStream() {
+    const broadcast = () => { }
+    const dispatch = () => { }
+
+    return {
+        broadcast,
+        dispatch
+    }
+}
+
+export abstract class HookFactory<
+    Args extends ArgsType<unknown[]> = [],
+    Return = ReturnType<undefined>
+> {
+    protected call(...args: Args) { }
+}
+
+export class AsyncHook extends HookFactory {
+}
+
+export class SyncHook extends HookFactory {
 }
