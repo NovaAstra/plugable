323d9514cb9e73a21af0b643ec6a5b9da6fa380c
NovaAstra <NovaAstra@outlook.com>
2024-08-07 22:14:19 +0800

    main

--- index.ts
+++ index.ts
@@ -1,59 +1,9 @@
-// @ts-nocheck
+export type MaybeAsync<T> = T | Promise<T>;
 
-export type ReturnType<T = undefined> = T extends Promise<infer R> ? Promise<R> : T;
+export type Args<T extends unknown[] = unknown[]> = T;
 
-export type ArgsType<T = unknown[]> = T extends unknown[] ? T : never;
+export type Return<T = void> = MaybeAsync<T>
 
-export type Callback<I = unknown, O = unknown> = (input: I) => O
+export type Hook<I extends unknown[] = [], O = undefined> = (...args: Args<I>) => Return<O>;
 
-export type Callbacks<I = unknown, O = unknown> = Callback<I, O>[];
-
-export type CounterCallback<I = unknown, O = unknown> = (index: number, input: I) => O
-
-export type Counter<I = unknown, O = unknown> = {
-    initiate: (input: I) => O
-    dispatch: (index: number, input: I) => O
-}
-
-export class CounterFactory<I, O> implements Counter<I, O> {
-    private callback: CounterCallback<I, O>
-
-    public constructor(callback: CounterCallback<I, O>) {
-        this.callback = callback
-    }
-
-    public initiate(input: I) {
-        return this.dispatch(0, input)
-    }
-
-    public dispatch(index: number, input: I) {
-        return this.callback(index, input)
-    }
-}
-
-export class HookFactory {
-    public callbacks = [];
-
-    public tap(callback) {
-        this.callbacks.push(callback)
-    }
-
-    public call(...args) {
-        const counter = new CounterFactory((index, input) => {
-            return this.callbacks[index].apply(this, input);
-        })
-
-        counter.initiate(args)
-    }
-}
-
-const event = new HookFactory()
-
-event.tap((...args) => {
-    console.log(args)
-})
-event.tap((...args) => {
-    console.log(args)
-})
-
-event.call("a", "b")
+export type Hooks<I extends unknown[] = [], O = undefined> = Hook<I, O>[]
