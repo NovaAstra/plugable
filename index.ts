fd540fb73e65a245ac6aed3aa4a2aec029281789
AugustGao <August.Gao@mspbots.ai>
2024-08-07 19:03:26 +0800

    main

--- index.ts
+++ index.ts
@@ -1,26 +1,59 @@
-export type ReturnType<T> = T extends Promise<infer R> ? Promise<R> : T;
+// @ts-nocheck
 
-export type ArgsType<T> = T extends unknown[] ? T : never;
+export type ReturnType<T = undefined> = T extends Promise<infer R> ? Promise<R> : T;
 
-export function createStream() {
-    const broadcast = () => { }
-    const dispatch = () => { }
+export type ArgsType<T = unknown[]> = T extends unknown[] ? T : never;
 
-    return {
-        broadcast,
-        dispatch
+export type Callback<I = unknown, O = unknown> = (input: I) => O
+
+export type Callbacks<I = unknown, O = unknown> = Callback<I, O>[];
+
+export type CounterCallback<I = unknown, O = unknown> = (index: number, input: I) => O
+
+export type Counter<I = unknown, O = unknown> = {
+    initiate: (input: I) => O
+    dispatch: (index: number, input: I) => O
+}
+
+export class CounterFactory<I, O> implements Counter<I, O> {
+    private callback: CounterCallback<I, O>
+
+    public constructor(callback: CounterCallback<I, O>) {
+        this.callback = callback
+    }
+
+    public initiate(input: I) {
+        return this.dispatch(0, input)
+    }
+
+    public dispatch(index: number, input: I) {
+        return this.callback(index, input)
     }
 }
 
-export abstract class HookFactory<
-    Args extends ArgsType<unknown[]> = [],
-    Return = ReturnType<undefined>
-> {
-    protected call(...args: Args) { }
+export class HookFactory {
+    public callbacks = [];
+
+    public tap(callback) {
+        this.callbacks.push(callback)
+    }
+
+    public call(...args) {
+        const counter = new CounterFactory((index, input) => {
+            return this.callbacks[index].apply(this, input);
+        })
+
+        counter.initiate(args)
+    }
 }
 
-export class AsyncHook extends HookFactory {
-}
+const event = new HookFactory()
 
-export class SyncHook extends HookFactory {
-}
+event.tap((...args) => {
+    console.log(args)
+})
+event.tap((...args) => {
+    console.log(args)
+})
+
+event.call("a", "b")
